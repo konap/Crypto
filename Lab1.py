37d504aff68ba0bf32389ce7e5719f04552b857a
konap <ko.9898@yandex.ua>
2019-03-02 07:53:33 +0200

    Reading from textfile

---
+++ Lab1.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+with open('text.txt') as f:
+    c = Counter()
+    for x in f:
+        c += Counter(x.strip())
+print (c)
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+"Lab#1 Entropy of text" 
